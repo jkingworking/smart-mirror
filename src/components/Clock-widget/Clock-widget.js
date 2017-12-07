03a60537935759862716c8a5836c215fb3011403
Jonathan King <jonathan@jkingworking.com>
2017-12-07 12:21:46 -0600

    fixed noon time issue and added globe hologram

--- src/components/Clock-widget/Clock-widget.js
+++ src/components/Clock-widget/Clock-widget.js
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types';
 import leftPad from 'left-pad';
 import Widget from '../Widget';
 // import background from './background-alt.gif';
-import backgroundMov from './background-alt.mp4';
+// import backgroundMov from './background-alt.mp4';
+import backgroundMov from './globe.mp4';
 
 function ClockWidget ({className, date}) {
 	const hrs = date.getHours();
@@ -14,7 +15,7 @@ function ClockWidget ({className, date}) {
       </video>
 			<h1 className="clock">
 				<span className="clock__hrs">
-					{hrs ? hrs < 12 ? hrs : hrs - 12 : 12}
+					{hrs ? hrs <= 12 ? hrs : hrs - 12 : 12}
 				</span>
 				<span className="clock__separator">:</span>
 				<span className="clock__min">
